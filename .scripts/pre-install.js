998401c1f8efb5b4b3a88853b7bec3e116bf25b3
Asaki Asako <asakiasako@foxmail.com>
2020-05-24 01:56:16 +0800

    update, change pipenv->poetry

--- .scripts/pre-install.js
+++ .scripts/pre-install.js
@@ -1,11 +1,6 @@
 const fs = require('fs')
 const path = require('path')
 
-/* Create .venv folder */
-// when .venv folder exists, pipenv will generate virtual env here
-venvPath = path.join(__dirname, '../py-api/.venv')
-if (!fs.existsSync(venvPath)) fs.mkdirSync(venvPath)
-
 /* Set node-gyp path, avoid 403 handle issue of current default node-gyp used by node (v5.0.5)*/
 const gypPath = path.join(__dirname, '../node_modules/node-gyp/bin/node-gyp.js')
 const npmrcPath = path.join(__dirname, '../.npmrc')
--- src/bg/rpc-server/const.js
+++ src/bg/rpc-server/const.js
@@ -1,9 +1,9 @@
 /* global __static */
 const path = require('path')
-// select rpc-server port
+// select rpc-server port (if occupied, will try += 1)
 const DEFAULT_SERVER_PORT = 23300
-const PYTHON_PATH = process.env.NODE_ENV !== 'production' ? path.join(__dirname, '../py-api/.venv/Scripts/python.exe') : null
-const PROCESS_PATH = process.env.NODE_ENV !== 'production' ? path.join(__dirname, '../py-api/main.py') : path.join(__static, 'api', 'api.exe')
+const PYTHON_PATH = process.env.NODE_ENV !== 'production' ? path.join(__dirname, '../py-code/.venv/Scripts/python.exe') : null
+const PROCESS_PATH = process.env.NODE_ENV !== 'production' ? path.join(__dirname, '../py-code/src/__main__.py') : path.join(__static, 'ElectronPythonSubProcess', 'ElectronPythonSubProcess.exe')
 
 module.exports = {
   DEFAULT_SERVER_PORT,
